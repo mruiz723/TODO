1b8dc9ae4f87ce4255184e45646413c7b9fc56a9
mruiz723 <mruiz723@gmail.com>
2016-10-23 21:03:57 -0500

    Create TaskCell, TaskViewController and NewTaskViewController.

---
+++ TODO/TODO/TaskViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TaskViewController.swift
+//  TODO
+//
+//  Created by Luis F Ruiz Arroyave on 10/23/16.
+//  Copyright © 2016 UdeM. All rights reserved.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
